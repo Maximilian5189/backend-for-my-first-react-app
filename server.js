a52742e6c47ac827f7dbf4f9430aa352e6cdbc51
Maximilian Schmidt <maximilian@Maximilians-MacBook-Pro.local>
2020-07-06 15:15:22 +0200

    added https

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const http = require('http');
+const https = require('https');
 const app = require('./app');
+const fs = require('fs');
 
 const port = process.env.PORT || 8080;
 
-const server = http.createServer(app);
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/mybackend.hopto.org/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/mybackend.hopto.org/fullchain.pem')
+  };
+
+const server = https.createServer(options, app);
 
 server.listen(port);
