5f92a649fce2ddfc3bf1e300effa83117bc596ba
Maximilian Schmidt <maximilian@Maximilians-MacBook-Pro.local>
2020-07-20 15:52:22 +0200

    use letsencrypt for production only

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const https = require('https');
+const http = require('http');
 const app = require('./app');
 const fs = require('fs');
 
 const port = process.env.PORT || 8000;
 
-const options = {
+let server;
+if(process.env.NODE_ENV === 'production') {
+  const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/mybackend.hopto.org/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/mybackend.hopto.org/fullchain.pem')
   };
 
-const server = https.createServer(options, app);
+server = https.createServer(options, app);
+} else {
+ server = http.createServer(app);
+}
 
 server.listen(port);
